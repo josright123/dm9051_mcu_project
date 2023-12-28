d72b89909e5bead29da14f0d67ff0d5ad5278849
vincent8572 <vwang3795@gmail.com>
2023-12-28 17:23:41 +0800

    Reapply "sdk32f415v2.0.9 porting to sdk32f413v2.1.1 <hello_chip & hello_link>"
    This reverts commit f9eef5f6553eba4564a94f4ba90e35480593fa42.

---
+++ sdk32f413v2.1.1_dm9051_hello_chip/developer/developer2/chip_5.2P/src/testproc/dm9051_lw_testproc.h
@@ -0,0 +1,22 @@
+/*
+ * dm9051_lw_testproc.h
+ */
+
+/* [Network test partner ip address] */
+#define DECLARATION_PARTNER_IPADDR  		"192.168.6.50"
+
+/* [DM9051 device mac address] */
+#if (ETHERNET_COUNT == 1) 
+static const uint8_t MACaddr[6] = {0x00, 0x60, 0x6e, 0x00, 0x01, 0x25};
+#endif
+
+//for 'main'
+void testproc_board_initialize(void);
+uint16_t testproc_drv_initialize(void);
+void testproc_net_test(void);
+void testproc_run(void);
+
+extern int test_line7_enter_check_setup; //.
+void self_test_tx_and_rx(void);
+
+void line7_proc(void);
